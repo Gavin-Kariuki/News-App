2aa714599bc8e54cd4af3de4149ac0cf039053d4
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-31 23:56:35 +0300

    add my configurations

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    ''''''
+
+    MY_API_KEY = os.environ.get('MY_API_KEY')
+    MY_BASE_URL = 'https://newsapi.org'
+    ARTICLES = MY_BASE_URL + \
+        '/v2/everything?sources={}&apikey='+str(MY_API_KEY)
+    SOURCES = MY_BASE_URL + \
+        '/v2/top-headlines/sources?category={}&apikey=' + str(MY_API_KEY)
+
+
+class ProdConfig(Config):
+
+    '''Production configuration class'''
+
+    pass
+
+
+class DevConfig(Config):
+    '''Development configuration class'''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
